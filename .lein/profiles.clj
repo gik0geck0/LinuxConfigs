e886fdd3c2b375fd28331069b18e7cedfe25f290
Matt Buland <gik0geck0@gmail.com>
2014-10-16 22:42:45 -0600

    Add leiningen profile, some brief xmobar updates, and add the urxvtd systemd service

---
+++ .lein/profiles.clj
@@ -0,0 +1,3 @@
+{:user {:plugins [ [lein-droid "0.2.3"] ]
+        :android {:sdk-path "/opt/android-sdk"}
+        }}
--- linker.sh
+++ linker.sh
@@ -5,4 +5,4 @@ ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.xmonad ~/.xmonad
 ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/.Xresources ~/.Xresources
-
+ln -s $PWD/.lein ~/.lein
